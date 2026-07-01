7838ce474135717081fdce397a6ca72f3689213a
shreyansh72962 <shreyansh729926@gmail.com>
2026-07-01 19:01:23 +0530

    Create sumoftwonumbers.cpp

---
+++ day1/sumoftwonumbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    cout << "Please enter value 1: ";
+    cin >> x;
+    cout << "Please enter value 2: ";
+    cin >> y;
+    cout << "Sum of the given values: " << (x + y) << endl;
+    return 0;
+}
